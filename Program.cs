736d268e9b8fc79de9a234751887d68ecc097c26
kohima399 <ikingagario@gmail.com>
2025-09-23 11:28:33 +0300

    Ввод числа с клавиатуры

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Введи X = ");
+        double x = Convert.ToDouble(Console.ReadLine());
     }
 }
